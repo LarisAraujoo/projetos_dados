d68d3cc8138804218788e6b4d5072748d721cfd5
Larissa Araujo <lari_araujo093@hotmail.com>
2026-03-12 19:27:58 -0300

    create

--- scripts/analise_vendas.py
+++ scripts/analise_vendas.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
 dados = pd.read_csv('dados/vendas.csv')
+dados["faturamento"] = dados["preco"] * dados["quantidade"]
 
 print(dados)
+
+faturamento_total = dados["faturamento"].sum()
+
+print("faturamento total:", faturamento_total)
